6a7a6dd82789cf59b8e001ee9ef8db37c80f7fcb
Roy <rdg01834@gmail.com>
2020-10-08 04:42:07 +0600

    adding new gym project

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,7 +20,7 @@ class Footer extends Component {
 
            <ul className="copyright">
               <li>&copy;Credit goes to Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Rahul DG</a></li>
+              <li>Design by <a title="Styleshout" href="https://github.com/RoyDG">Rahul DG</a></li>
            </ul>
 
         </div>
